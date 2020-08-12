20dc4ace96f21a212428731837aa43462d01ab4f
Colgate13 <54006467+Colgate13@users.noreply.github.com>
2020-08-12 16:18:44 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-    res.send("<h1>Servidor rodando com ExpressJS</h1>");
+    res.send("<h1>EU TE AMO LILI</h1>");
 });
 
 http.createServer(app).listen(21184, () => console.log("Servidor rodando local na porta 3000"));
