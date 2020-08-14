b5a2d47106712579557e8ca25f32723acba2c708
Colgate13 <54006467+Colgate13@users.noreply.github.com>
2020-08-14 11:48:16 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,70 @@
-const http = require("http");
+var http = require('http')
+var express = require('express')
 
-const express = require("express");
-const app = express();
+app = express();
+
+var porta = 21012
+ 
+var path = require('path');
+
+
+//const {  pageLanding, saveClasses } = require(__dirname + '/pages.js')
+
 
 app.get("/", function(req, res) {
-    res.send("<h1>EU TE AMO LILI</h1>");
+    res.sendFile(__dirname + '/views/index.html'); //http://mh.kinghost.net/home
 });
 
-http.createServer(app).listen(21184, () => console.log("Servidor rodando local na porta 3000"));
+app.get("/study", function(req, res) {
+    res.sendFile(__dirname + '/views/study.html'); //http://mh.kinghost.net/home/study
+});
+
+app.get("/give-classes", function(req, res) {
+    res.sendFile(__dirname + '/views/give-classes.html'); //http://mh.kinghost.net/home/study
+});
+//app.get("/teste", pageLanding) 
+
+
+
+
+//PASTA PUBLIC
+//---------------------------------------------------------------------------------[
+//ROTAS PARA ARQUIVOS STYLES , IMGS E SCRIPT. 
+
+app.get("/public/scripts/:tagName", function(req, res) {
+  res.download(path.join(__dirname, '../public/scripts', req.params.tagName))
+});
+
+//ROTA PARA ICONE DO SITE
+app.get("/public/top-icon", function(req, res) {
+  res.download(path.join(__dirname, '../public/images/icons', 'purple-heart.svg'))
+});
+
+//Rota para CSS MAIN
+app.get("/public/styles/main.css", function(req, res) {
+ res.sendFile(path.join(__dirname, '../public/styles', 'main.css'))
+});
+
+
+//Rota para CSS 
+app.get("/public/styles/:tagName", function(req, res) {
+  res.download(path.join(__dirname, '../public/styles/partials', req.params.tagName))
+});
+
+//Rota para arquivos imagens icons
+app.get("/public/images/icons/:tagName", function(req, res) {
+  res.download(path.join(__dirname, '../public/images/icons', req.params.tagName))
+});
+
+//Rota para arquivos imagens
+app.get("/public/images/:tagName", function(req, res) {
+  res.download(path.join(__dirname, '../public/images/', req.params.tagName))
+});
+//------------------------------------------------------------------------------]
+
+
+var server = app.listen(21012);
+
+console.log('Servidor rodando na porta: '+ porta)
+console.log(__dirname + '/views/index.html')
+
